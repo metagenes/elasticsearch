898e4be1285f818af75e80b5913dc04432688023
yudi prayoga <yudi@technik.id>
2020-08-31 17:14:54 +0700

    init upload

--- application/controllers/Search.php
+++ application/controllers/Search.php
@@ -1,10 +1,11 @@
+
 <?php
 use Restserver\Libraries\REST_Controller;
 use Elasticsearch\ClientBuilder;
 require 'vendor\autoload.php';
 defined('BASEPATH') or exit('No direct script access allowed');
 require APPPATH . '/libraries/REST_Controller.php';
-class Product extends REST_Controller
+class Search extends REST_Controller
 {
     public function __construct()
     {
@@ -12,27 +13,21 @@ class Product extends REST_Controller
         //inisialisasi model Produk_model.php dengan nama produk
         $this->load->database();
     }
-    public function index_get()
-    {
-        $client = ClientBuilder::create()->build();
-        $params = [
-            'index' => 'pet_name',
-            'id' => 'test_id'
-        ];
-        
-        $response = $client->get($params);
-        $this->response($response);
-    }
     public function index_post()
     {
         $client = ClientBuilder::create()->build();
         $params = [
-            'index' => $this->post('index'),
-            'id'    => rand(),
-            'body'  => ['testField' => $this->post('body')]
+            'index' => 'pet_name',
+            'body'  => [
+                'query' => [
+                    'match' => [
+                        'testField' => $this->post('body')
+                    ]
+                ]
+            ]
         ];
         
-        $response = $client->index($params);
+        $response = $client->search($params);
         $this->response($response);
     }
 }
